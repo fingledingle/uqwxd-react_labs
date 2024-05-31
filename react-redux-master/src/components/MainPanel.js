2a4e49764cdcdf3b3b179cc7fd24b3de56d6a103
fingledingle <elenai@riseup.net>
2024-05-31 23:21:05 +0900

    idk

--- react-redux-master/src/components/MainPanel.js
+++ react-redux-master/src/components/MainPanel.js
@@ -1,1 +1,17 @@
+import React from 'react'
+import Mybutton from './MyButton'
+import DivPanel from './DivPanel'
 
+
+
+const MainPanel = ()=> {
+    return (
+        <div>
+            This is main pale <Mybutton></Mybutton>
+            <DivPanel></DivPanel>
+        </div>
+    );
+}
+
+
+export default MainPanel;
--- react-redux-master/src/components/MyButton.js
+++ react-redux-master/src/components/MyButton.js
@@ -1,1 +1,14 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import increment from '../actions'
 
+
+const Mybutton = () => {
+    let dispatch = useDispatch();
+    return (
+        <button onClick={() => dispatch(increment(1))}>Increase Counter</button>
+    );
+
+}
+
+export default Mybutton;
